19d5a26569d89b8c47137d695012a16d2557d4db
Jackson Medeiros <jackson.dgmedeiros@gmail.com>
2025-03-11 15:52:15 -0300

    adicionando informações do restcontroller, com um CRUD completo

--- src/main/java/com/dio/spring_security/spring_security/WelcomeController.java
+++ src/main/java/com/dio/spring_security/spring_security/WelcomeController.java
@@ -1,14 +1,22 @@
 package com.dio.spring_security.spring_security;
 
+import com.dio.spring_security.spring_security.model.User;
+import com.dio.spring_security.spring_security.repository.UsersRespository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class WelcomeController {
 
+    @Autowired
+    private UsersRespository usersRespository;
+
     @GetMapping
     public String welcome() {
+
         return "Welcome to Spring Security";
     }
     @GetMapping("/users")
@@ -22,4 +30,27 @@ public class WelcomeController {
     public String managers() {
         return "You are logged in manager";
     }
+
+    @GetMapping("/todos")
+    public List<User> getUsers(){
+        return usersRespository.findAll();
+    }
+
+//    PathVariable, tras o que passou como parametro na URI
+    @GetMapping("/todos/{username}")
+    public User getOne(@PathVariable("username") String user){
+        return usersRespository.findByUsername(user);
+    }
+
+    @DeleteMapping("/users/{id}")
+    public void delete(@PathVariable("id") Integer id){
+        usersRespository.deleteById(id);
+    }
+
+    @PostMapping("/users")
+    public User postUser(@RequestBody User user){
+        usersRespository.save(user);
+        return user;
+    }
+
 }
--- src/main/java/com/dio/spring_security/spring_security/repository/UsersRespository.java
+++ src/main/java/com/dio/spring_security/spring_security/repository/UsersRespository.java
@@ -4,10 +4,24 @@ import com.dio.spring_security.spring_security.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Repository
 public interface UsersRespository extends JpaRepository<User, Integer> {
 
     @Query("SELECT e FROM User e JOIN FETCH e.roles WHERE e.userName = (:username)")
     public User findByUsername(@Param("username") String username);
+
+    public default List<User> listarTodos(){
+        List<User> users = new ArrayList<>();
+        users.add(new User("jackson", "senha", new Object()));
+
+        return users;
+    }
+
 
 }
